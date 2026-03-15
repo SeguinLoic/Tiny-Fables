2f410a4dcef5a75729cc19621b707e380a9c07e6
Trevor Lee <49603972+trevortylerlee@users.noreply.github.com>
2026-03-15 16:07:42 -0700

    Astro v6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Geist",
+      cssVariable: "--font-geist",
+    },
+  ],
   markdown: {
     shikiConfig: {
       theme: "dark-plus",
